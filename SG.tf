ebe0a7531fed44eca704bdd3dc2b466276bf462f
NoelOcasio <ocasio889@gamail.com>
2025-04-23 12:09:29 -0500

    Adding webserver security group

--- SG.tf
+++ SG.tf
@@ -43,3 +43,33 @@ resource "aws_security_group" "load_balancer_sg" {
     Name = "LoadBalancerSecurityGroup"
   }
 }
+
+resource "aws_security_group" "web_server_sg" {
+  name        = "Web Server Security Group"
+  description = "Web Server Access"
+  vpc_id      = aws_vpc.project_vpc.id
+
+  # Inbound rule for HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [
+      aws_security_group.load_balancer_sg.id
+    ]
+    description = "Allow HTTP Inbound from Load Balancer"
+  }
+
+  # Outbound rules (allow all by default)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name = "WebServerSecurityGroup"
+  }
+}
